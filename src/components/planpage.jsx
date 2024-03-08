50e30fb02927fce26abe7e70c6970fb770775468
Athul Bhandary <athulbhandary28@gmail.com>
2024-03-08 22:45:51 +0530

    Made the page responsive

--- src/components/planpage.jsx
+++ src/components/planpage.jsx
@@ -4,11 +4,12 @@ export default function PlanPage() {
     return (
         <div>
             <Navbar home={true} about={true} plan={false} />
-            <div className="muscle-gain text-white font-kod h-[700px] relative p-10">
-                <p className="text-4xl absolute top-[30%] sm:right-[35%]">MUSCLE GAIN FOCUS</p>
-                <p className="absolute left-[15%] top-[50%] text-[#74f0ed] text-2xl uppercase w-[300px]">Gain muscle mass while having a low body fat percentage </p>
-                <button className="plans-button p-5 rounded-3xl text-black absolute left-[20%] bottom-16">CREATE PLAN</button>
+            <div className="muscle-gain text-white font-kod h-[700px] relative p-10 pl-20 ">
+                <p className="lg:text-6xl text-5xl absolute top-[45%] max-lg:left-[15%] md:right-[25%] ">MUSCLE GAIN FOCUS</p>
+                <p className="absolute left-[15%] top-[25%] text-[#74f0ed] text-2xl uppercase w-[300px]">Gain muscle mass while having a low body fat percentage </p>
+                <button className="plans-button p-5 rounded-3xl text-xl text-black absolute left-[15%] bottom-28">CREATE PLAN</button>
             </div>
+
         </div>
     );
 }
