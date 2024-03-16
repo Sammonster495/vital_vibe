57aa04f4d0841f2ca412cf512d3618ecd3880098
Sammonster495 <samarthshetty495@gmail.com>
2024-03-16 12:19:41 +0530

    feat: add muscle gain page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/homepage";
 import AboutPage from "./components/aboutpage";
 import PlanPage from "./components/planpage";
-import Muscle from "./components/muscle";
+import MuscleGain from "./components/musclegain";
 
 export default function App(){
     return (
@@ -12,7 +12,7 @@ export default function App(){
                     <Route path="/" element={<HomePage />} />
                     <Route path="/about" element={<AboutPage />} />
                     <Route path="/plan" element={<PlanPage />} />
-                    <Route path="/predict" element={<Muscle />} />
+                    <Route path="/plan/musclegain" element={<MuscleGain />} />
                 </Routes>
             </Router>
         </div>
